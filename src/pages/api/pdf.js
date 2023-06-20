708b588e4f32f074d2ef50ecd4e9c392fe134324
Grechileme <grechilemelucas@gmail.com>
2023-06-20 08:31:20 -0400

    Switch from html-pdf to jsPDF

--- src/pages/api/pdf.js
+++ src/pages/api/pdf.js
@@ -1,22 +1,39 @@
-import pdf from 'html-pdf';
-import fs from 'fs';
+// import pdf from 'html-pdf';
+// import fs from 'fs';
 
-export default async function handler (req, res) {
+// export default async function handler (req, res) {
 
 
-    const html = "<html><body><h1>Hello there</h1></body></html>";
+//     const html = "<html><body><h1>Hello there</h1></body></html>";
 
-    const options = { format: 'Letter' };
+//     const options = { format: 'Letter' };
 
-    pdf.create(html, options).toStream((err, stream) => {
-        if (err) {
-            res.status(500).send(err);
-            console.log('err');
-            console.log(err)
-        }
-        else {
-            res.setHeader('Content-Type', 'application/pdf');
-            stream.pipe(res);
-        }
-    });
+//     pdf.create(html, options).toStream((err, stream) => {
+//         if (err) {
+//             res.status(500).send(err);
+//             console.log('err');
+//             console.log(err)
+//         }
+//         else {
+//             res.setHeader('Content-Type', 'application/pdf');
+//             stream.pipe(res);
+//         }
+//     });
+// }
+
+// UP TO HERE WAS MADE USING HTML-PDF, WORKED LOCALLY, NOT SO MUCH IN PRODUCTION
+
+import jsPDF from "jspdf";
+
+export default async function handler(req, res) {
+    const doc = new jsPDF();
+
+    doc.setFontSize(16);
+    doc.text('Hello there', 10, 10);
+
+    const pdfBuffer = doc.output('arraybuffer');
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.send(Buffer.from(pdfBuffer));
+    
 }
